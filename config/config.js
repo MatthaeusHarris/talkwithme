8658c8c108c70a20a2cde3fab5fa4473abb31455
Matt Harris <matthaeus.harris@gmail.com>
2015-04-18 10:59:01 -0700

    Add (ugly) UI support for user languages

---
+++ config/config.js
@@ -0,0 +1,20 @@
+/**
+ * Created by matt on 4/18/15.
+ */
+
+module.exports = {
+    languages: {
+        english: {
+            charset: 'UTF-8',
+            displayName: 'English'
+        },
+        spanish: {
+            charset: 'UTF-8',
+            displayName: 'Spanish'
+        },
+        french: {
+            charset: 'UTF-8',
+            displayName: 'French'
+        }
+    }
+};
--- routes/user.js
+++ routes/user.js
@@ -6,27 +6,52 @@ var router = express.Router();
 var auth = require('../lib/auth');
 var User = require('../models/user');
 var flash = require('../lib/flash');
+var config = require('../config/config');
 
 router.get('/user', auth.ensureAuthenticated, function(req, res) {
-    res.render('user', {
+    var templateVars = {
         authId: req.user.authId,
         name: req.user.name,
         email: req.user.email,
         role: req.user.role,
         created: req.user.created,
-        nickname: req.user.nickname
-    });
+        nickname: req.user.nickname,
+        knownLanguages: [],
+        wantedLanguages: [],
+        languageList: config.languages
+    };
+    for (var language in config.languages) {
+        templateVars.knownLanguages.push({
+            languageName: language,
+            displayName: config.languages[language].displayName,
+            checked: req.user.languages.known.indexOf(language) !== -1 ? 'checked' : ''
+        });
+        templateVars.wantedLanguages.push({
+            languageName: language,
+            displayName: config.languages[language].displayName,
+            checked: req.user.languages.wanted.indexOf(language) !== -1 ? 'checked' : ''
+        });
+    }
+    //console.log(req.user);
+    //console.log(templateVars);
+    res.render('user', templateVars);
 });
 
 router.post('/user', auth.ensureAuthenticated, function(req, res) {
+    console.log(req.body);
     var query = {
         authId: req.user.authId
     };
     var update = {
         name: req.body.name,
         email: req.body.email,
-        nickname: req.body.nickname
+        nickname: req.body.nickname,
+        languages: {
+            known: Object.keys(req.body['language-known']),
+            wanted: Object.keys(req.body['language-wanted'])
+        }
     };
+    console.log(update);
     User.findOneAndUpdate(query, update, function(err, user) {
         if (err || !user) {
             flash(req, {
